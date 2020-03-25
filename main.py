180ef1907b35148c2b09f55912d162647d16d47a
alyssa-adams <amadam15@asu.edu>
2020-03-24 21:48:07 -0500

    Updated TODO list

---
+++ main.py
@@ -0,0 +1,3 @@
+# TODO: Need to make sure all the directories self-create
+# TODO: Need to make sure all dependencies can auto-install, cut out any unnecessary ones
+# TODO: Make the main script, so it can run from the command line
